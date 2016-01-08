88e2e5fe1e8c274123f19d0ec301fa2a6ccd87b9
Jean-Philippe Braun <eon@patapon.info>
2016-01-08 20:00:09 +0100

    Fix API change

--- contrail_api_cli_extra/service_appliance_set.py
+++ contrail_api_cli_extra/service_appliance_set.py
@@ -9,10 +9,10 @@ class AddSAS(Command):
 
     def __call__(self, appliance_set_name=None, driver=None):
         global_config = Resource('global-system-config',
-                                 fq_name='default-global-system-config')
+                                 fq_name='default-global-system-config',
+                                 check_fq_name=True)
         sas = Resource('service-appliance-set',
-                       fq_name='default-global-system-config:%s' % appliance_set_name,
-                       check_fq_name=False)
+                       fq_name='default-global-system-config:%s' % appliance_set_name)
         sas['parent_type'] = 'global-system-config'
         sas['parent_uuid'] = global_config.uuid
         sas['service_appliance_driver'] = driver
