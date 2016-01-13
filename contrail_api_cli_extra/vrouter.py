8acdbef326b83080cb758112bd7a4c6f3b584c13
Jean-Philippe Braun <eon@patapon.info>
2016-01-13 15:32:18 +0100

    Split vrouter in add-vrouter and del-vrouter

--- contrail_api_cli_extra/vrouter.py
+++ contrail_api_cli_extra/vrouter.py
@@ -12,47 +12,46 @@ from .utils import ip_type
 
 
 class VRouter(Command):
-    description = 'Add or remove vrouters'
-    oper = Arg(help='Add or remove vrouter',
-               choices=['add', 'del'])
-    vrouter_name = Arg('--vrouter-name',
-                       help='Hostname of compute node',
-                       required=True)
+    vrouter_name = Arg(help='Hostname of compute node')
+
+
+class AddVRouter(VRouter):
+    description = 'Add vrouter'
     vrouter_ip = Arg('--vrouter-ip',
                      help='IP of compute node',
-                     type=ip_type)
+                     type=ip_type,
+                     required=True)
     vrouter_type = Arg('--vrouter-type',
                        help='vrouter type',
                        choices=['tor-service-mode', 'embedded'],
                        default=None)
 
-    def __call__(self, oper=None, controllers=None, vrouter_ip=None,
-                 vrouter_name=None, vrouter_type=None):
+    def __call__(self, vrouter_ip=None, vrouter_name=None, vrouter_type=None):
 
-        if oper == 'add':
-            if not vrouter_ip:
-                raise CommandError('--vrouter-ip mandatory to add a vrouter')
-            global_config = Resource('global-system-config',
-                                     fq_name='default-global-system-config',
-                                     check_fq_name=True)
+        global_config = Resource('global-system-config',
+                                 fq_name='default-global-system-config',
+                                 check_fq_name=True)
+        vrouter = Resource('virtual-router',
+                           fq_name='default-global-system-config:%s' % vrouter_name,
+                           parent_type='global-system-config',
+                           parent_uuid=global_config.uuid,
+                           virtual_router_ip_address=vrouter_ip)
+        if vrouter_type:
+            vrouter['virtual_router_type'] = [vrouter_type]
+        try:
+            vrouter.save()
+        except HttpError as e:
+            raise CommandError(text_type(e))
+
+
+class DelVRouter(VRouter):
+    description = 'Remove vrouter'
+
+    def __call__(self, vrouter_name=None):
+        try:
             vrouter = Resource('virtual-router',
                                fq_name='default-global-system-config:%s' % vrouter_name,
-                               parent_type='global-system-config',
-                               parent_uuid=global_config.uuid,
-                               virtual_router_ip_address=vrouter_ip)
-            if vrouter_type:
-                vrouter['virtual_router_type'] = [vrouter_type]
-            try:
-                vrouter.save()
-            except HttpError as e:
-                raise CommandError(text_type(e))
-
-        elif oper == 'del':
-            try:
-                vrouter = Resource('virtual-router',
-                                   fq_name='default-global-system-config:%s' % vrouter_name,
-                                   check_fq_name=True)
-            except ValueError as e:
-                raise CommandError(text_type(e))
-
-            vrouter.delete()
+                               check_fq_name=True)
+        except ValueError as e:
+            raise CommandError(text_type(e))
+        vrouter.delete()
--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ setup(
             'linklocal = contrail_api_cli_extra.linklocal:Linklocal',
             'route-target = contrail_api_cli_extra.route_target:RouteTarget',
             'set-encaps = contrail_api_cli_extra.encapsulation:SetEncapsulation',
-            'vrouter = contrail_api_cli_extra.vrouter:VRouter',
+            'add-vrouter = contrail_api_cli_extra.vrouter:AddVRouter',
+            'del-vrouter = contrail_api_cli_extra.vrouter:DelVRouter',
         ]
     },
     classifiers=[
