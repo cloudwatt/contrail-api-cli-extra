9c0eb3d2539fa939088d6f9e53a0c6cf742864dd
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2016-05-26 17:20:15 +0200

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='contrail-api-cli-extra',
-    version='0.2.1',
+    version='0.2.2',
     description="Supplementary commands for contrail-api-cli",
     author="Jean-Philippe Braun",
     author_email="eon@patapon.info",
