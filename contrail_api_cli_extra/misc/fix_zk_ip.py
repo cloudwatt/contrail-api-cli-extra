d979b0236fb70d60464134e026a9d55b4892a338
Mathieu Turpin <mathieu.turpin@orange.com>
2016-08-02 10:58:01 +0200

    merge _get_subnet and _get_subnet_for_ip

--- contrail_api_cli_extra/misc/fix_zk_ip.py
+++ contrail_api_cli_extra/misc/fix_zk_ip.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals, print_function
 from netaddr import IPNetwork, IPAddress, AddrFormatError
 from contrail_api_cli.command import Option
@@ -7,10 +6,13 @@ from contrail_api_cli.utils import continue_prompt
 from contrail_api_cli.exceptions import(CommandError,
                                         ResourceNotFound)
 
+import gevent.monkey
 import logging
+import ipdb
 
 from ..utils import ZKCommand, CheckCommand, PathCommand
 
+gevent.monkey.patch_socket()
 logger = logging.getLogger(__name__)
 
 
@@ -27,75 +29,69 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
         return ('/api-server/subnets/{0}:{1}/{2}'
                 .format(fqname, prefix, prefix_len))
 
-    def _zk_node_for_ip(self, vn, ip):
+    def _zk_node_for_ip(self, _vn, _ip):
+        logger.debug("check IP {0} in virtual network {1}"
+                     .format(_ip, _vn))
         try:
-            vn.fetch()
-        except ResourceNotFound:
-            raise ResourceNotFound('virtual-network {} not found.'
-                                   .format(str(vn)))
-            return
-        try:
-            subnets = self._get_subnet(vn)
-        except CommandError:
-            msg = ('No subnet found for virtual_network {0}'.format(str(vn)))
-            logger.info(msg)
-            print(msg)
-            return
-        try:
-            ip_subnet = self._get_subnet_for_ip(ip, subnets)
+            ip_subnet = self._get_subnet(_vn, ip=_ip)
+            if type(ip_subnet) == 'list':
+                ipdb.set_trace()
         except ResourceNotFound:
             raise ResourceNotFound('No suitable subnet found for IP {0}'
                                    'in virtual_network {1}'
-                                   .format(str(ip), str(vn)))
+                                   .format(str(_ip), str(_vn)))
         return ('/api-server/subnets/{0}:{1}/{2}/{3}'
-                .format(vn.fq_name,
+                .format(_vn.fq_name,
                         ip_subnet.network,
                         ip_subnet.prefixlen,
-                        int(ip)))
+                        int(_ip)))
 
-    def _get_subnet(self, virtual_network):
-        subnet = []
+    def _get_subnet(self, virtual_network, ip=None):
         vn_subnet = (virtual_network['network_ipam_refs'][0]
                                     ['attr']['ipam_subnets'])
-        for s in vn_subnet:
-            prefix = s['subnet']['ip_prefix']
-            prefix_len = s['subnet']['ip_prefix_len']
-            subnet.append((IPNetwork(str(prefix) + '/' + str(prefix_len))))
-        if subnet:
-            return subnet
+        if vn_subnet:
+            subnet = []
+            for s in vn_subnet:
+                prefix = s['subnet']['ip_prefix']
+                prefix_len = s['subnet']['ip_prefix_len']
+                subnet.append((IPNetwork(str(prefix) + '/' + str(prefix_len))))
         else:
             raise ResourceNotFound('No subnet defined for virtual_network {0}'
-                                   .format(str(virtual_network)))
+                                   .format(str(virtual_network.fq_name)))
+        if ip:
+            ip = IPAddress(ip)
+            for s in subnet:
+                if ip in s:
+                    if str(ip) == '0.0.0.0':
+                        ipdb.set_trace()
+                    return s
+            raise ResourceNotFound('No subnet in virtual_network {0} is valid'
+                                   'for IP {1}'
+                                   .format(str(virtual_network.fq_name),
+                                           str(ip)))
+        else:
+            return subnet
 
-    def _get_subnet_for_ip(self, ip, subnets):
-        for subnet in subnets:
-            if ip in subnet:
-                return subnet
-        raise CommandError('Subnet not found for IP {0} in subnet(s) {1}'
-                           .format(str(ip), subnets))
-
-    def get_zk_ip(self, virtual_network):
+    def get_zk_ip(self, vn):
         zk_ips = {}
-        try:
-            virtual_network.fetch()
-        except ResourceNotFound:
-            return zk_ips
-        fq_name = str(virtual_network.fq_name)
-        if len(virtual_network['network_ipam_refs']
-                              [0]['attr']['ipam_subnets']) == 0:
-            msg = ('No subnet defined for {0}'.format(fq_name))
+        if not vn['network_ipam_refs'][0]['attr']['ipam_subnets']:
+            msg = ('No subnet defined for {0}'.format(str(vn.fq_name)))
             logger.info(msg)
             print(msg)
             return zk_ips
-        for s in self._get_subnet(virtual_network):
+        try:
+            subnets = self._get_subnet(vn)
+        except ResourceNotFound:
+            return zk_ips
+        for s in subnets:
             zk_subnet_req = self._zk_node_for_subnet(
-                                fq_name,
+                                str(vn.fq_name),
                                 str(s.network),
                                 str(s.prefixlen)
                             )
             for _ip in self.zk_client.get_children(zk_subnet_req):
                 try:
-                    zk_ip = IPAddress(_ip)
+                    zk_ip = IPAddress(int(_ip))
                 except AddrFormatError:
                     msg = ('{0} INVALID IP : zk_path={1}/{2}'
                            .format(str(_ip), zk_subnet_req, str(_ip)))
@@ -103,7 +99,7 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                     print(msg)
                     continue
                 try:
-                    self._get_subnet_for_ip(zk_ip, [s])
+                    self._get_subnet(vn, ip=zk_ip)
                 except CommandError:
                     msg = ('{0} OUT OF RANGE {1} : zk_path={2}/{3}'
                            .format(str(zk_ip),
@@ -113,19 +109,15 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                     logger.warning(msg)
                     print(msg)
                     continue
-                zk_ips[str(zk_ip)] = zk_subnet_req + '/' + str(_ip)
+                zk_ips[str(zk_ip)] = zk_subnet_req + '/' + str(int(zk_ip))
         return zk_ips
 
     def get_api_ip(self, vn):
         api_ips = {}
-        try:
-            vn.fetch()
-        except ResourceNotFound:
-            return api_ips
-        if len(vn['network_ipam_refs'][0]['attr']['ipam_subnets']) == 0:
+        if not vn['network_ipam_refs'][0]['attr']['ipam_subnets']:
             msg = ('No subnet defined for {0}'.format(vn))
             logger.info(msg)
-            print (msg)
+            print(msg)
             return api_ips
         vn_subnet = vn['network_ipam_refs'][0]['attr']['ipam_subnets']
         for s in vn_subnet:
@@ -140,6 +132,7 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
             if 'dns_server_address' in s:
                 api_ips[str(IPAddress(
                         s['dns_server_address']))] = vn
+
         if 'instance_ip_back_refs' in vn:
             for iip in vn.back_refs.instance_ip:
                 try:
@@ -181,14 +174,14 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                     api_ips[index] = fip
         return api_ips
 
-    def create_znode(self, zk_req, data, dry_run=True):
+    def create_znode(self, zk_req, data):
         if self.zk_client.exists(str(zk_req)):
             msg = ('{0} allready exists'.format(zk_req))
             logger.info(msg)
             print(msg)
             self.stats['miss_lock_fixed'] += 1
             return
-        if not dry_run:
+        if not self.dry_run:
             try:
                 self.zk_client.create(zk_req,
                                       value=str(data),
@@ -198,38 +191,37 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                 self.stats['miss_lock_fix_failed'] += 1
                 msg = ('Unable to create zookeeper znode {0}'
                        .format(zk_req,))
-                CommandError(msg)
+                raise CommandError(msg)
 
-    def del_znode_ip(self, ip, zk_path, dry_run=True):
-            msg = ("deleting zookeeper node {0} for IP {1}"
-                   .format(int(IPAddress(ip)), str(IPAddress(ip))))
-            logger.info(msg)
-            if not self.dry_run:
-                try:
-                    self.zk_client.delete(zk_path)
-                    self.stats['abusive_lock_fixed'] += 1
-                except:
-                    self.stats['abusive_lock_fix_failed'] += 1
-                    CommandError('Unable to delete zookeeper znode for ip '
-                                 '{0} with path {1}'
-                                 .format(str(IPAddress(ip)), str(zk_path)))
-            return
+    def del_znode_ip(self, ip, zk_path):
+        msg = ("deleting zookeeper node {0} for IP {1}"
+               .format(int(IPAddress(ip)), str(IPAddress(ip))))
+        logger.info(msg)
+        if not self.dry_run:
+            try:
+                self.zk_client.delete(zk_path)
+                self.stats['abusive_lock_fixed'] += 1
+            except:
+                self.stats['abusive_lock_fix_failed'] += 1
+                raise CommandError('Unable to delete zookeeper znode for ip '
+                                   '{0} with path {1}'
+                                   .format(str(IPAddress(ip)), str(zk_path)))
+        return
 
-    def add_ip_lock(self, vn, ip, data_lock, dry_run):
-        if not IPAddress(ip):
-            raise CommandError('Invalid IP {0}'.format(ip))
-            return
+    def add_ip_lock(self, vn, ip, data_lock):
         try:
-            vn.fetch()
+            zk_req = self._zk_node_for_ip(vn, ip)
         except ResourceNotFound:
+            self.stats['miss_lock_fix_failed'] += 1
             return
-        zk_req = self._zk_node_for_ip(vn, ip)
-        logger.info('Creating zookeeper node {0} for IP {1}'
-                    .format(str(zk_req), str(ip)))
-        if not dry_run:
-            self.create_znode(zk_req, data_lock, dry_run)
+        msg = ('Creating zookeeper node {0} for IP {1}'
+               .format(str(zk_req), str(ip)))
+        logger.info(msg)
+        print(msg)
+        if not self.dry_run:
+            self.create_znode(zk_req, data_lock)
 
-    def add_znode_ip(self, _ip, resource, dry_run=True):
+    def add_znode_ip(self, _ip, resource):
         try:
             resource.fetch()
         except ResourceNotFound:
@@ -249,25 +241,25 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                 vn.fetch()
             except ResourceNotFound:
                 return
-            data = resource.uuid
+            data_lock = resource.uuid
         elif resource.type == 'instance-ip':
             try:
                 vn = resource['virtual_network_refs'][0]
                 vn.fetch()
             except ResourceNotFound:
                 return
-            data = vn.uuid
+            data_lock = vn.uuid
         elif resource.type == 'virtual-network':
             try:
                 vn = resource
                 vn.fetch()
             except ResourceNotFound:
                 return
-            data = vn.uuid
+            data_lock = vn.uuid
         else:
-            print('Unknow type for ip {0}'.format(ip))
+            raise ResourceNotFound('Unknow type for ip {0}'.format(ip))
             return
-        self.add_ip_lock(vn, ip, data, dry_run)
+        self.add_ip_lock(vn, ip, data_lock)
 
     def check_tupple(self, api_ips, zk_ips):
         ips = {}
@@ -279,15 +271,22 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
             ips[ip] = {}
             if ip in zk_ips:
                 ips[ip].update({'zk_path': zk_ips[ip]})
-                logger.info(str(ip) + ' : ZOOKEEPER OK')
+                msg = (str(ip) + ' : ZOOKEEPER OK')
+                logger.info(msg)
                 zk_ok = True
             else:
                 logger.info(str(ip) + ' : NOT FOUND IN ZOOKEEPER')
                 self.stats['miss_lock'] += 1
                 if not self.check:
-                    self.add_znode_ip(ip,
-                                      api_ips[ip],
-                                      dry_run=self.dry_run)
+                    try:
+                        self.add_znode_ip(ip, api_ips[ip])
+                    except (ResourceNotFound,
+                            AddrFormatError,
+                            CommandError) as e:
+                        msg = e.msg
+                        logger.warning(e)
+                        print(msg)
+
             if ip in api_ips:
                 ips[ip].update({'resource': api_ips[ip]})
                 logger.info(str(ip) + ' : API OK')
@@ -296,7 +295,12 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
                 logger.info(str(ip) + ' : NOT FOUND IN API')
                 self.stats['abusive_lock'] += 1
                 if not self.check:
-                    self.del_znode_ip(ip, zk_ips[ip], dry_run=self.dry_run)
+                    try:
+                        self.del_znode_ip(ip, zk_ips[ip])
+                    except ResourceNotFound as e:
+                        msg = e.msg
+                        logger.error(msg)
+                        print(msg)
             if zk_ok and api_ok:
                 self.stats['healthy_lock'] += 1
         return
@@ -348,8 +352,8 @@ class FixZkIP(ZKCommand, CheckCommand, PathCommand):
             except ResourceNotFound:
                 continue
             if 'network_ipam_refs' in r:
-                zk_ips = self.get_zk_ip(r)
                 api_ips = self.get_api_ip(r)
+                zk_ips = self.get_zk_ip(r)
             else:
                 logger.info('No subnet found for network {0} '
                             .format(str(r.fq_name)))
