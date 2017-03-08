f36d9170469ac9bbf25567c73428ddec6a091601
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2017-03-08 17:44:55 +0100

    check-bad-refs: allow to delete incomplete references

--- contrail_api_cli_extra/misc/check_bad_refs.py
+++ contrail_api_cli_extra/misc/check_bad_refs.py
@@ -4,12 +4,13 @@ import json
 
 from pycassa import ConnectionPool, ColumnFamily
 
-from contrail_api_cli.command import Command, Option, Arg
+from contrail_api_cli.command import Option, Arg
 from contrail_api_cli.resource import Resource
 from contrail_api_cli.exceptions import ResourceNotFound
-from contrail_api_cli.utils import printo, highlight_json, parallel_map
+from contrail_api_cli.utils import printo, highlight_json, \
+    parallel_map, continue_prompt
 
-from ..utils import server_type
+from ..utils import server_type, CheckCommand
 
 
 class PropertiesEncoder(json.JSONEncoder):
@@ -20,7 +21,7 @@ class PropertiesEncoder(json.JSONEncoder):
         return json.JSONEncoder.encode(self, o)
 
 
-class CheckBadRefs(Command):
+class CheckBadRefs(CheckCommand):
     """Check for broken references.
 
     The command will read all objects from the cassandra DB then
@@ -33,12 +34,14 @@ class CheckBadRefs(Command):
         contrail-api-cli check-bad-refs --cassandra-servers db:9160 [uuids...]
     """
     description = "Check for broken references"
-    uuids = Arg(help="Check specific uuids",
+    uuids = Arg(help="check specific uuids",
                 nargs="*", default=[])
-    cassandra_servers = Option(help="Cassandra server list' (default: %(default)s)",
+    cassandra_servers = Option(help="cassandra server list' (default: %(default)s)",
                                nargs='+',
                                type=server_type,
                                default=['localhost:9160'])
+    force = Option('-f', help="force deletion of incomplete resources",
+                   action="store_true", default=False)
 
     def _props_to_json(self, values):
         if self.is_piped:
@@ -71,7 +74,14 @@ class CheckBadRefs(Command):
         results = parallel_map(self._check_ref, to_check, args=(uuid,), workers=20)
         return any(results)
 
-    def __call__(self, uuids=None, cassandra_servers=None):
+    def _delete(self, uuid_cf, uuid):
+        if not self.dry_run:
+            uuid_cf.remove(uuid)
+        printo("[%s] deleted" % uuid)
+
+    def __call__(self, uuids=None, cassandra_servers=None, force=False, **kwargs):
+        super(CheckBadRefs, self).__call__(**kwargs)
+        self.force = force
         pool = ConnectionPool('config_db_uuid', server_list=cassandra_servers)
         uuid_cf = ColumnFamily(pool, 'obj_uuid_table')
         if uuids:
@@ -89,3 +99,6 @@ class CheckBadRefs(Command):
             bad_refs = self._check_resource_refs(uuid, values)
             if not res or bad_refs:
                 printo(self._props_to_json(values))
+            if not res and not self.check:
+                if self.force or continue_prompt(message="Delete ?"):
+                    self._delete(uuid_cf, uuid)
