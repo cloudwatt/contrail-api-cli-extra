6230a2c04af696f1bc0400db2f0608ac0af05a6e
rasta-rocket <bruno.fernandos@yahoo.com>
2018-04-16 17:10:33 +0200

    clean-refs: Add conditions to enable check mode

--- contrail_api_cli_extra/clean/refs.py
+++ contrail_api_cli_extra/clean/refs.py
@@ -59,15 +59,18 @@ class CleanRefs(CheckCommand):
         source, target = paths[:2]
         # when the parent doesn't exists anymore,
         # we don't need to keep the source
-        if self.ref_type == "parent":
-            self.uuid_cf.remove(target)
-            self.uuid_cf.remove(source)
-        else:
-            self.uuid_cf.remove(target)
-            self.uuid_cf.remove(source, columns=['%s:%s:%s' % (self.ref_type, self.target_type, target)])
+        if not self.check:
+            if self.ref_type == "parent":
+                self.uuid_cf.remove(target)
+                self.uuid_cf.remove(source)
+            else:
+                self.uuid_cf.remove(target)
+                self.uuid_cf.remove(source, columns=['%s:%s:%s' % (self.ref_type, self.target_type, target)])
 
         printo("[%s -> %s] deleted" % (source, target))
-        self._remove_refs(paths[2:])
+
+        if not self.check:
+            self._remove_refs(paths[2:])
 
     def __call__(self, paths=None, ref_type=None, target_type=None, cassandra_servers=None, **kwargs):
         super(CleanRefs, self).__call__(**kwargs)
